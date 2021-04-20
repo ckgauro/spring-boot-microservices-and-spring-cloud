5383b23ecbc798c1bbc29ff0ae60b79503ad7f3b
Chandra kishor Gauro <ckgauro@gmail.com>
2021-04-19 22:35:34 -0400

    Eureka Client

--- photoappapiusers/Notes/Section10.md
+++ photoappapiusers/Notes/Section10.md
@@ -1,8 +1,9 @@
-# chapter 68
+# chapter 68  --Done
 
-mvn spring-boot:run -Dspring-boot.run.arguemnts=--spring.application.instance_id=chand
+mvn spring-boot:run -Dspring-boot.run.arguments=--spring.application.instance_id=chand
+mvn spring-boot:run -Dspring-boot.run.arguments="--spring.application.instance_id=chand2 --server.port=8999"
 
-mvn spring-boot:run -Dspring-boot.run.arguemnts=--spring.application.instance_id=chand2,--server.port=8999
+mvn spring-boot:run -Dspring-boot.run.arguments="--server.port=8999 --spring.application.instance_id=dhn"
 
 --------------
 # chapter 69
--- photoappapiusers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ photoappapiusers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,6 +1,6 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
-import org.checkerframework.common.reflection.qual.GetClass;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    private final Environment environment;
+
+    public UsersController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status/check")
     public String status(){
-        return "working users";
+        return "working on port "+environment.getProperty("local.server.port")+", with token ="+environment.getProperty("token.secret");
     }
 }
